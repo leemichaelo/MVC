c79d8a18c123fa9160fd03e2bbf4f2b1c6fe5260
Lee Owen <LeeMichaelO@outlook.com>
2018-06-06 18:52:03 -0700

    Added Comic Books List View

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -79,5 +79,10 @@ namespace ComicBookGallery.Data
             }
             return comicBookToReturn;
         }
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
     }
 }
