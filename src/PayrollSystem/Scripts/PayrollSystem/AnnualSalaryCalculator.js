39c0c362d95968258e628878c21369c79b550b81
Lee Owen <LeeMichaelO@outlook.com>
2018-07-01 08:45:22 -0700

    Initial Push with Week 1 Completed

---
+++ src/PayrollSystem/Scripts/PayrollSystem/AnnualSalaryCalculator.js
@@ -0,0 +1,6 @@
+﻿function annualSalaryCalculation() {
+    var weeklyWages = document.getElementById("hourlyWages").value;
+    var hoursPerWeek = document.getElementById("hoursPerWeek").value;
+    var numberOfWeeks = document.getElementById("numberOfWeeks").value;
+    document.getElementById("annualCalculation").innerHTML = "Annual Salary: $" + (weeklyWages * hoursPerWeek * numberOfWeeks).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
