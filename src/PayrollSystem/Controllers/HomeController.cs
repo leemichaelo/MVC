220fea29556f4decf29b0bac808baf81e587fea7
Lee-Michael Owen <LeeMichaelO@outlook.com>
2018-07-08 10:02:39 -0700

    Updated the annualsalary view to allow stepping through decimals on the hourly wages and the hours per week

--- src/PayrollSystem/Controllers/HomeController.cs
+++ src/PayrollSystem/Controllers/HomeController.cs
@@ -16,24 +16,29 @@ namespace PayrollSystem.Controllers
 
         public ActionResult AnnualSalaryCalculator()
         {
+            //Initializes the salary with the default values
             var annualSalary = new AnnualSalaryCalculator()
             {
                 HourlyWages = 00.00,
                 HoursPerWeek = 0
             };
 
+            //When the page loads it starts with the default values
             return View(annualSalary);
         }
 
         [HttpPost]
         public ActionResult AnnualSalaryCalculator(AnnualSalaryCalculator annualSalary)
         {
+            //Validates the values against the object
             if (ModelState.IsValid)
             {
+                //When the user hits the calculate button in the view, this calculates the annual salary
                 ViewBag.AnnualSalary = (annualSalary.HourlyWages * annualSalary.HoursPerWeek) * annualSalary.WeeksPerYear;
                 return View();
             }
 
+            //If the model state is not valid it will keep the values on the page
             return View(annualSalary);
         }
 
